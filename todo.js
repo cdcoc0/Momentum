752eab226542e5004216fbab159a71fb8e6d74f5
velykitty <jiyunee13@gmail.com>
2020-10-11 03:14:49 +0900

    todo-2 -function saveToDos >save todos in the storage -function loadToDos >get each item from the storage and display on the browser

--- todo.js
+++ todo.js
@@ -4,15 +4,30 @@ toDoList = document.querySelector(".js-toDoList");
 
 const TODOS_LS = "toDos";
 
+const TODOS = [];
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(TODOS));
+    //스토리지에는 string만 저장 가능
+}
+
 function paintToDo(text) {
     const LI = document.createElement("li");
     const DELBTN = document.createElement("button");
-    DELBTN.innerText = "❌"; //win + .
     const SPAN = document.createElement("span");
+    const newId = TODOS.length + 1;
+    DELBTN.innerText = "❌"; //win + .
     SPAN.innerText = text;
     LI.appendChild(SPAN);
     LI.appendChild(DELBTN);
+    LI.id = newId;
     toDoList.appendChild(LI);
+    const toDoOjb = {
+        text: text, 
+        id: newId
+    };
+    TODOS.push(toDoOjb);
+    saveToDos(); //배열에 먼저 저장을 해야 스토리지에 저장할 요소가 생성됨
 }
 
 function handleSubmit(event) {
@@ -23,9 +38,12 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS);
-    if(toDos !== null) {
-
+    const loadedToDos = localStorage.getItem(TODOS_LS);
+    if(loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos);
+        parsedToDos.forEach(function(toDO) {
+            paintToDo(toDO.text);
+        });
     }
 }
 
